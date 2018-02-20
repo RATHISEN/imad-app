707aa28c1d72636bfd5e39a640f00cda555c0610
RATHISEN <36510600+RATHISEN@users.noreply.github.com>
2018-02-20 10:21:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ var element = document.getElementById('maintext');
 element.innerHTML='new value';
 var pic=document.getElementById('pic');
 pic.onClick = function (){
-    pic.style.marginLeft='200px';
+    pic.style.marginLeft='60px';
 }
 
