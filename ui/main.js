3f59718080c9017954247ff35ac7f84d8d744f9c
RATHISEN <36510600+RATHISEN@users.noreply.github.com>
2018-02-20 10:19:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById('maintext');
 element.innerHTML='new value';
+var pic=document.getElementById('pic');
+pic.onClick = function (){
+    pic.style.marginLeft='100px';
+}
+
